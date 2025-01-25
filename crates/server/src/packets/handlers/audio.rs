31fcb659e8b6ec090f6ede2f358cb16a85e2389e
lukeskydiamonds <me@gfanedm.dev>
2025-01-25 15:31:50 +0100

    Refactor audio packet handling to streamline client communication and improve test structure

--- crates/server/src/packets/handlers/audio.rs
+++ crates/server/src/packets/handlers/audio.rs
@@ -5,7 +5,7 @@ use crate::{
     packets::{PacketData, PacketHandler},
     server::client::Clients,
 };
-use common::packet::{ids::PacketId, packet_type::PacketType, AudioPacket};
+use common::packet::ids::PacketId;
 
 pub struct AudioHandler(pub Arc<Clients>);
 
@@ -16,26 +16,16 @@ impl PacketHandler for AudioHandler {
             return Err(ServerError::InvalidHandlerPacketId);
         }
 
-        let packet: AudioPacket =
-            AudioPacket::decode(&data.packet).map_err(|_| ServerError::InvalidPacket)?;
-        println!("Processing audio packet: {:?}", packet);
-
-        {
-            for client in self.0.lock().await.values() {
-                if client.id() != data.client_id {
-                    match client.send(&data.packet).await {
-                        Ok(_) => {
-                            println!("{:?} -> {:?}", data.client_id, client.id());
-                        }
-                        Err(e) => {
-                            return Err(e);
-                        }
-                    };
-                }
+        for client in self.0.lock().await.values() {
+            if client.id() != data.client_id {
+                client.send(&data.packet).await?;
+                println!(
+                    "🎙️ {:.8} -> {:.8}",
+                    data.client_id.to_string(),
+                    client.id().to_string()
+                );
             }
         }
-
-        println!("Processed audio packet: {:?}", packet);
 
         Ok(())
     }
@@ -47,50 +37,70 @@ mod tests {
 
     use super::*;
     use ::tokio::sync::{mpsc, Mutex};
-    use common::packet::ids::PacketId;
+    use common::packet::{ids::PacketId, packet_type::PacketType, AudioPacket};
     use std::collections::HashMap;
+    use tokio::select;
     use uuid::Uuid;
 
     #[tokio::test]
     async fn test_audio_handler() {
         let clients = Arc::new(Mutex::new(HashMap::new()));
-        let (tx, mut _read_tx) = mpsc::channel(1);
+        let (tx, mut read_tx) = mpsc::channel(1);
+        let (tx_2, mut read_tx_2) = mpsc::channel(1);
+
+        let client = Client::new(Uuid::new_v4(), tx);
+        let second_client = Client::new(Uuid::new_v4(), tx_2);
+
         {
+            clients.lock().await.insert(client.id(), client);
             clients
                 .lock()
                 .await
-                .insert(Uuid::new_v4(), Client::new(Uuid::new_v4(), tx));
+                .insert(second_client.id(), second_client);
         }
+
+        let audio_packet = AudioPacket {
+            track: vec![1, 2, 3, 4, 5],
+        }
+        .encode()
+        .unwrap();
 
         assert!(
             AudioHandler(clients)
                 .process(PacketData::new(
                     Default::default(),
                     PacketId::AudioPacket,
-                    AudioPacket {
-                        track: vec![1, 2, 3, 4, 5]
-                    }
-                    .encode()
-                    .unwrap()
+                    audio_packet.clone(),
                 ))
                 .await
                 .is_ok(),
             "Expected handler to process packet"
         );
+
+        select! {
+            result = tokio::spawn(async move { read_tx.recv().await }) => {
+                assert!(result.is_ok());
+                assert_eq!(result.unwrap().unwrap(), audio_packet, "Expected packet to be sent to first client");
+            }
+            result = tokio::spawn(async move { read_tx_2.recv().await }) => {
+                assert!(result.is_ok());
+                assert_eq!(result.unwrap().unwrap(), audio_packet, "Expected packet to be sent to second client");
+            }
+        }
     }
 
     #[tokio::test]
     async fn test_audio_handler_invalid_packet_id() {
-        // assert!(
-        //     AudioHandler {}
-        //         .process(PacketData::new(
-        //             Default::default(),
-        //             PacketId::ConnectPacket,
-        //             AudioPacket::default().encode().unwrap()
-        //         ))
-        //         .await
-        //         .is_err(),
-        //     "Expected handler to return error for invalid packet id"
-        // );
+        assert!(
+            AudioHandler(Arc::new(Mutex::new(HashMap::new())))
+                .process(PacketData::new(
+                    Default::default(),
+                    PacketId::ConnectPacket,
+                    AudioPacket::default().encode().unwrap()
+                ))
+                .await
+                .is_err(),
+            "Expected handler to return error for invalid packet id"
+        );
     }
 }
